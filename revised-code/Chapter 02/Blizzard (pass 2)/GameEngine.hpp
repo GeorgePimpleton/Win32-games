b2a5fa4fa3145b0823be183d64407c23b10b87dc
George Pimpleton <george_pimpleton@hotmail.com>
2022-08-04 12:24:31 -0700

    Update Chapter 2: Blizzard, pass 2

---
+++ revised-code/Chapter 02/Blizzard (pass 2)/GameEngine.hpp
@@ -0,0 +1,77 @@
+//-----------------------------------------------------------------
+// Game Engine Object
+// C++ Header - GameEngine.hpp
+//-----------------------------------------------------------------
+
+#pragma once
+
+//-----------------------------------------------------------------
+// Include Files
+//-----------------------------------------------------------------
+#include <windows.h>
+#include <strsafe.h>
+
+const static UCHAR str_length { 64 };
+
+//-----------------------------------------------------------------
+// Windows Function Declarations
+//-----------------------------------------------------------------
+int WINAPI        wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                           _In_ PWSTR szCmdLine, _In_ int iCmdShow);
+LRESULT CALLBACK  WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//-----------------------------------------------------------------
+// Game Engine Function Declarations
+//-----------------------------------------------------------------
+HRESULT GameInitialize(HINSTANCE hInstance);
+void    GameStart(HWND hWindow);
+void GameEnd();
+void GameActivate(HWND hWindow);
+void GameDeactivate(HWND hWindow);
+void GamePaint(HDC hDC);
+void GameCycle();
+
+//-----------------------------------------------------------------
+// GameEngine Class
+//-----------------------------------------------------------------
+class GameEngine
+{
+protected:
+   // Member Variables
+   static GameEngine* m_pGameEngine;
+   HINSTANCE           m_hInstance;
+   HWND                m_hWindow;
+   WCHAR               m_szWindowClass[ str_length ];
+   WCHAR               m_szTitle[ str_length ];
+   WORD                m_wIcon;
+   WORD                m_wSmallIcon;
+   int                 m_iWidth;
+   int                 m_iHeight;
+   int                 m_iFrameDelay;
+   BOOL                m_bSleep;
+
+public:
+   // Constructor(s)/Destructor
+            GameEngine(HINSTANCE hInstance, PCWSTR szWindowClass, PCWSTR szTitle,
+                       WORD wIcon, WORD wSmallIcon, int iWidth = 640, int iHeight = 480);
+   virtual ~GameEngine();
+
+   // General Methods
+   static GameEngine* GetEngine() { return m_pGameEngine; };
+   HRESULT            Initialize(int iCmdShow);
+   LRESULT            HandleEvent(HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam);
+
+   // Accessor Methods
+   HINSTANCE GetInstance()                { return m_hInstance; };
+   HWND      GetWindow()                  { return m_hWindow; };
+   void      SetWindow(HWND hWindow)      { m_hWindow =  hWindow; };
+   PCWSTR    GetTitle()                   { return m_szTitle; };
+   WORD      GetIcon()                    { return m_wIcon; };
+   WORD      GetSmallIcon()               { return m_wSmallIcon; };
+   int       GetWidth()                   { return m_iWidth; };
+   int       GetHeight()                  { return m_iHeight; };
+   int       GetFrameDelay()              { return m_iFrameDelay; };
+   void      SetFrameRate(int iFrameRate) { m_iFrameDelay = 1000 / iFrameRate; };
+   BOOL      GetSleep()                   { return m_bSleep; };
+   void      SetSleep(BOOL    bSleep)     { m_bSleep = bSleep; };
+};
