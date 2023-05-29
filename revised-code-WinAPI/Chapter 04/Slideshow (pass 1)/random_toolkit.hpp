6f4045c99a08c3a080341838019448c56812c518
George Pimpleton <george_pimpleton@hotmail.com>
2023-05-28 17:12:56 -0700

    Updated Random Toolkit to V 1.3

--- revised-code-WinAPI/Chapter 04/Slideshow (pass 1)/random_toolkit.hpp
+++ revised-code-WinAPI/Chapter 04/Slideshow (pass 1)/random_toolkit.hpp
@@ -2,8 +2,10 @@
  *
  * pre-C++20 header file */
 
- // shamelessly stolen and adapted from a C++ working paper: WG21 N3551
- // http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3551.pdf
+ // v1.3
+
+  // shamelessly stolen and adapted from a C++ working paper: WG21 N3551
+  // http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3551.pdf
 
 #ifndef RANDOM_TOOLKIT
 #define RANDOM_TOOLKIT
@@ -17,7 +19,7 @@ namespace rtk
 {
    static bool seeded { false };
 
-   inline std::mt19937& urng()
+   inline std::mt19937& urng( )
    {
       static std::mt19937 URNG { };
 
@@ -28,15 +30,15 @@ namespace rtk
    // uses a seed sequence for better randomization
    inline void srand(bool FORCE_SEED = false)
    {
-      static const std::seed_seq::result_type seeds[] { std::random_device {} (),
-                                                        std::seed_seq::result_type(std::chrono::system_clock::now().time_since_epoch().count()) };
+      static const std::seed_seq::result_type seeds[ ] { std::random_device {} (),
+                                                         std::seed_seq::result_type(std::chrono::system_clock::now( ).time_since_epoch( ).count( )) };
 
       static std::seed_seq sseq(std::begin(seeds), std::end(seeds));
 
       // the URNG can't be reseeded unless forced
       if ( !seeded || FORCE_SEED )
       {
-         urng().seed(sseq);
+         urng( ).seed(sseq);
 
          seeded = true;
       }
@@ -49,34 +51,45 @@ namespace rtk
       // the URNG can't be reseeded unless forced
       if ( !seeded || FORCE_SEED )
       {
-         urng().seed(static_cast<unsigned>(seed));
+         urng( ).seed(static_cast<unsigned>(seed));
 
          seeded = true;
       }
    }
 
-   // two function overloads to obtain uniform distribution ints and doubles
-   inline int rand(int from, int to)
+   // two functions to obtain uniform distribution ints and doubles
+   inline int rand_int(int from, int to)
    {
       static std::uniform_int_distribution<> dist { };
 
       if ( from > to ) { throw std::invalid_argument("bad int distribution params"); }
 
-      return dist(urng(), decltype(dist)::param_type { from, to });
+      return dist(urng( ), decltype(dist)::param_type { from, to });
    }
 
-   inline double rand(double from, double to)
+   inline double rand_rl(double from, double to)
    {
       static std::uniform_real_distribution<> dist { };
 
       // a real distribution kinda goes flakey when the params are equal, divide by zero will do that,
       // as well as reversed from expected
-      if ( from >= to || ((to - from) > std::numeric_limits<double>::max()) )
+      if ( !(from < to && ((to - from) <= std::numeric_limits<double>::max( ))) )
       {
          throw std::invalid_argument("bad double distribution params");
       }
 
-      return dist(urng(), decltype(dist)::param_type { from, to });
+      return dist(urng( ), decltype(dist)::param_type { from, to });
+   }
+
+   // two function overloads to obtain uniform distribution ints and doubles
+   inline int rand(int from, int to)
+   {
+      return rand_int(from, to);
+   }
+
+   inline double rand(double from, double to)
+   {
+      return rand_rl(from, to);
    }
 
    // function for rolling dice, and checking if the # of pips is nonstandard
