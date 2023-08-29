efd1f1ac54a908f888588cc572dd6c9c99fc5a96
George Pimpleton <george_pimpleton@hotmail.com>
2023-08-29 14:49:00 -0700

    code revision

--- revised-code-WinAPI/Chapter 13/Henway 2 (mod 1)/Henway 2.hpp
+++ revised-code-WinAPI/Chapter 13/Henway 2 (mod 1)/Henway 2.hpp
@@ -1,7 +1,3 @@
-// "Talk to me like I'm a 3 year old!" Programming Lessons - Windows Games
-
-// Henway 2.hpp - Game Header
-
 #pragma once
 
 #include <windows.h>
@@ -11,15 +7,15 @@
 #include "Sprite.hpp"
 #include "GameEngine.hpp"
 
-GameEngine* _pGame;
-Bitmap*     _pHighwayBitmap;
-Bitmap*     _pChickenBitmap;
-Bitmap*     _pCarBitmaps[4];
-Bitmap*     _pChickenHeadBitmap;
-BOOL        _bGameOver;
-HDC         _hOffscreenDC;
-HBITMAP     _hOffscreenBitmap;
-UINT        _iInputDelay;
-UINT        _iNumLives;
-UINT        _iScore;
-Sprite*     _pChickenSprite;
+GameEngine* g_game;
+Bitmap*     g_highwayBitmap;
+Bitmap*     g_chickenBitmap;
+Bitmap*     g_carBitmaps[4];
+Bitmap*     g_chickenHeadBitmap;
+BOOL        g_gameOver;
+HDC         g_offscreenDC;
+HBITMAP     g_offscreenBitmap;
+UINT        g_inputDelay;
+UINT        g_numLives;
+UINT        g_score;
+Sprite*     g_chickenSprite;
