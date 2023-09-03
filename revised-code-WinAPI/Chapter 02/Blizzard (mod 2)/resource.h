d1cb6f9c379a8004f17d5f3c1c6a4bd87d6312cf
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-03 11:36:39 -0700

    Blizzard updates

--- revised-code-WinAPI/Chapter 02/Blizzard (mod 2)/resource.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define IDI_ICON    1000
-#define IDI_ICON_SM 1001
--- revised-code-WinAPI/Chapter 02/Blizzard (mod 6)/Blizzard.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <memory>
-#include "random_toolkit.hpp"
-#include "resource.h"
-#include "GameEngine.hpp"
-
-std::unique_ptr<GameEngine> g_game;
