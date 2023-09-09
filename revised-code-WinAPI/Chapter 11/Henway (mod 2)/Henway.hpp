32714d00fbc3d56a534133018a3ff3e9d27d199b
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-09 11:53:56 -0700

    Henway

---
+++ revised-code-WinAPI/Chapter 11/Henway (mod 2)/Henway.hpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+#include <vector>
+#include "resource.h"
+#include "Bitmap.hpp"
+#include "Sprite.hpp"
+#include "GameEngine.hpp"
+#include "random_toolkit.hpp"
+
+GameEngine* g_game;
+Bitmap*     g_highwayBitmap;
+Bitmap*     g_chickenBitmap;
+Bitmap*     g_carBitmaps[4];
+Bitmap*     g_chickenHeadBitmap;
+BOOL        g_gameOver;
+HDC         g_offscreenDC;
+HBITMAP     g_offscreenBitmap;
+UINT        g_inputDelay;
+UINT        g_numLives;
+UINT        g_score;
+Sprite*     g_chickenSprite;
+
+void MoveChicken(int xDistance, int yDistance);
--- revised-code-WinAPI/README.md
+++ revised-code-WinAPI/README.md
@@ -22,12 +22,12 @@ Windows and C/C++ have changed since 2003/2004.  Programmers need to adapt.
 8. Example Game: Light Cycles
 9. Fore: Making Things Move with Sprites
 10. Fore 2: Managing a World of Sprites
+11. Example Game: Henway
 
 ![under construction](construction.gif)
 
 \*\* ***under construction*** \*\*
 
-11. Example Game: Henway
 12. Brainiac 2: Playing Digital Sound Effects/Wave Files
 13. Henway 2: Playing MIDI Music
 14. Example Game: Battle Office
