cb2a6db2eaf397ec933ad3c0cb6ba0093198a222
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-21 11:11:17 -0700

    Trippy

--- revised-code-WinAPI/Chapter 03/Trippy (part 1)/Trippy.cpp
+++ revised-code-WinAPI/Chapter 03/Trippy (part 1)/Trippy.cpp
@@ -1,86 +1,75 @@
-//-----------------------------------------------------------------
-// Trippy Application
-// C++ Source - Trippy.cpp
-//-----------------------------------------------------------------
-
-//-----------------------------------------------------------------
-// Include Files
-//-----------------------------------------------------------------
 #include "Trippy.h"
 
-//-----------------------------------------------------------------
-// Game Engine Functions
-//-----------------------------------------------------------------
 BOOL GameInitialize(HINSTANCE hInstance)
 {
-   // Create the game engine
-   _pGame = new GameEngine(hInstance, TEXT("Trippy"), TEXT("Trippy: Learning to Draw Basic Graphics"), IDI_TRIPPY, IDI_TRIPPY_SM);
-   if ( _pGame == NULL )
-      return FALSE;
+   g_pGame = new GameEngine(hInstance, TEXT("Trippy"), TEXT("Trippy: Learning to Draw Basic Graphics"),
+                            IDI_TRIPPY, IDI_TRIPPY_SM);
 
-   // Set the frame rate
-   _pGame->SetFrameRate(15);
+   if ( g_pGame == NULL )
+   {
+      return FALSE;
+   }
+
+   g_pGame->SetFrameRate(15);
 
    return TRUE;
 }
 
 void GameStart(HWND hWindow)
 {
-   // Seed the random number generator
    srand(GetTickCount( ));
 
-   // Set the position and size of the initial rectangle
-   _rcRectangle.left = _pGame->GetWidth( ) * 2 / 5;
-   _rcRectangle.top = _pGame->GetHeight( ) * 2 / 5;
-   _rcRectangle.right = _rcRectangle.left + _pGame->GetWidth( ) / 5;
-   _rcRectangle.bottom = _rcRectangle.top + _pGame->GetHeight( ) / 5;
+   g_rcRectangle.left   = g_pGame-> GetWidth( ) * 2 / 5;
+   g_rcRectangle.top    = g_pGame-> GetHeight( ) * 2 / 5;
+   g_rcRectangle.right  = g_rcRectangle.left + g_pGame-> GetWidth( ) / 5;
+   g_rcRectangle.bottom = g_rcRectangle.top + g_pGame-> GetHeight( ) / 5;
 }
 
 void GameEnd( )
 {
-   // Cleanup the game engine
-   delete _pGame;
+   delete g_pGame;
 }
 
 void GameActivate(HWND hWindow)
-{
-}
+{ }
 
 void GameDeactivate(HWND hWindow)
-{
-}
+{ }
 
 void GamePaint(HDC hDC)
 {
-   // Draw grid lines as a background for the rectangles
+   // draw grid lines as a background for the rectangles
    const int iGridLines = 50;
+
    for ( int i = 1; i <= iGridLines; i++ )
    {
-      // Draw a horizontal grid line
-      MoveToEx(hDC, _pGame->GetWidth( ) * i / iGridLines, 0, NULL);
-      LineTo(hDC, _pGame->GetWidth( ) * i / iGridLines, _pGame->GetHeight( ));
+      // draw a horizontal grid line
+      MoveToEx(hDC, g_pGame->GetWidth( ) * i / iGridLines, 0, NULL);
+      LineTo(hDC, g_pGame->GetWidth( ) * i / iGridLines, g_pGame->GetHeight( ));
 
       // Draw a vertical grid line
-      MoveToEx(hDC, 0, _pGame->GetHeight( ) * i / iGridLines, NULL);
-      LineTo(hDC, _pGame->GetWidth( ), _pGame->GetHeight( ) * i / iGridLines);
+      MoveToEx(hDC, 0, g_pGame->GetHeight( ) * i / iGridLines, NULL);
+      LineTo(hDC, g_pGame->GetWidth( ), g_pGame->GetHeight( ) * i / iGridLines);
    }
 }
 
 void GameCycle( )
 {
-   HDC         hDC;
-   HWND        hWindow = _pGame->GetWindow( );
-   HBRUSH      hBrush;
+   HDC    hDC;
+   HWND   hWindow = g_pGame-> GetWindow( );
+   HBRUSH hBrush;
 
-   // Randomly alter the position and size of the rectangle
+   // randomly alter the position and size of the rectangle
    int iInflation = (rand( ) % 21) - 10;
-   InflateRect(&_rcRectangle, iInflation, iInflation);
-   OffsetRect(&_rcRectangle, (rand( ) % 19) - 9, (rand( ) % 19) - 9);
 
-   // Draw the new rectangle in a random color
+   InflateRect(&g_rcRectangle, iInflation, iInflation);
+   OffsetRect(&g_rcRectangle, (rand( ) % 19) - 9, (rand( ) % 19) - 9);
+
+   // draw the new rectangle in a random color
    hBrush = CreateSolidBrush(RGB(rand( ) % 256, rand( ) % 256, rand( ) % 256));
-   hDC = GetDC(hWindow);
-   FillRect(hDC, &_rcRectangle, hBrush);
+   hDC    = GetDC(hWindow);
+
+   FillRect(hDC, &g_rcRectangle, hBrush);
    ReleaseDC(hWindow, hDC);
    DeleteObject(hBrush);
 }
