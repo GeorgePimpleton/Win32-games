22cd8d1cfec232e7c7229583868b6562b19c8bd7
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-22 06:02:11 -0700

    Slideshow

---
+++ revised-code-WinAPI/Chapter 04/Slideshow (part 4)/Bitmap.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <windows.h>
+
+class Bitmap
+{
+protected:
+   HBITMAP m_hBitmap;
+   int     m_width;
+   int     m_height;
+
+   void Free( );
+
+public:
+            Bitmap( );
+            Bitmap(PCWSTR fileName);
+            Bitmap(UINT resID, HINSTANCE inst);
+            Bitmap(HDC dc, int width, int height, COLORREF color = RGB(0, 0, 0));
+   virtual ~Bitmap( );
+
+   BOOL Create(PCWSTR fileName);
+   BOOL Create(UINT resID, HINSTANCE inst);
+   BOOL Create(HDC dc, int width, int height, COLORREF color);
+
+   void Draw(HDC dc, int x, int y);
+
+   int GetWidth( ) const  { return m_width; };
+   int GetHeight( ) const { return m_height; };
+};
