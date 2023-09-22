1ef26bc5fd0e40e3aed756f2190cc58702ff45c6
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-22 09:45:41 -0700

    UFO

---
+++ revised-code-WinAPI/Chapter 05/UFO (part 2)/Bitmap.cpp
@@ -0,0 +1,133 @@
+// include the WinAPI library for TransparentBlt()
+#pragma comment(lib, "msimg32.lib")
+
+#include "Bitmap.hpp"
+
+Bitmap::Bitmap( )
+   : m_bitmap(NULL), m_width(0), m_height(0)
+{
+}
+
+Bitmap::Bitmap(PCWSTR fileName)
+   : m_bitmap(NULL), m_width(0), m_height(0)
+{
+   Create(fileName);
+}
+
+Bitmap::Bitmap(UINT resID, HINSTANCE inst)
+   : m_bitmap(NULL), m_width(0), m_height(0)
+{
+   Create(resID, inst);
+}
+
+Bitmap::Bitmap(HDC dc, int width, int height, COLORREF color)
+   : m_bitmap(NULL), m_width(0), m_height(0)
+{
+   Create(dc, width, height, color);
+}
+
+Bitmap::~Bitmap( )
+{
+   Free( );
+}
+
+void Bitmap::Free( )
+{
+   if ( m_bitmap != NULL )
+   {
+      DeleteObject(m_bitmap);
+      m_bitmap = NULL;
+   }
+}
+
+BOOL Bitmap::Create(PCWSTR fileName)
+{
+   Free( );
+
+   m_bitmap = (HBITMAP) LoadImageW(NULL, fileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+
+   if ( m_bitmap == NULL )
+   {
+      Free( );
+      return FALSE;
+   }
+
+   BITMAP bitmap;
+
+   GetObjectW(m_bitmap, sizeof(BITMAP), &bitmap);
+
+   m_width  = bitmap.bmWidth;
+   m_height = bitmap.bmHeight;
+
+   return TRUE;
+}
+
+BOOL Bitmap::Create(UINT resID, HINSTANCE inst)
+{
+   Free( );
+
+   m_bitmap = (HBITMAP) LoadImageW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(resID), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+
+   if ( m_bitmap == NULL )
+   {
+      Free( );
+      return FALSE;
+   }
+
+   BITMAP bitmap;
+
+   GetObjectW(m_bitmap, sizeof(BITMAP), &bitmap);
+
+   m_width  = bitmap.bmWidth;
+   m_height = bitmap.bmHeight;
+
+   return TRUE;
+}
+
+BOOL Bitmap::Create(HDC dc, int width, int height, COLORREF color)
+{
+   m_bitmap = CreateCompatibleBitmap(dc, width, height);
+
+   if ( m_bitmap == NULL )
+   {
+      return FALSE;
+   }
+
+   m_width  = width;
+   m_height = height;
+
+   HDC     memDC     = CreateCompatibleDC(dc);
+   HBRUSH  brush     = CreateSolidBrush(color);
+   HBITMAP oldBitmap = (HBITMAP) SelectObject(memDC, m_bitmap);
+   RECT    bitmap    = { 0, 0, m_width, m_height };
+
+   FillRect(memDC, &bitmap, brush);
+
+   SelectObject(memDC, oldBitmap);
+   DeleteDC(memDC);
+   DeleteObject(brush);
+
+   return TRUE;
+}
+
+void Bitmap::Draw(HDC dc, int x, int y, BOOL trans, COLORREF transColor)
+{
+   if ( m_bitmap != NULL )
+   {
+      HDC     memDC     = CreateCompatibleDC(dc);
+      HBITMAP oldBitmap = (HBITMAP) SelectObject(memDC, m_bitmap);
+
+      if ( trans )
+      {
+         TransparentBlt(dc, x, y, GetWidth( ), GetHeight( ), memDC, 0, 0,
+                        GetWidth( ), GetHeight( ), transColor);
+      }
+      else
+      {
+         BitBlt(dc, x, y, GetWidth( ), GetHeight( ), memDC, 0, 0, SRCCOPY);
+      }
+
+      SelectObject(memDC, oldBitmap);
+      DeleteDC(memDC);
+   }
+}
---
+++ revised-code-WinAPI/Chapter 05/UFO (part 2)/UFO.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <windows.h>
+#include <memory>
+#include "resource.h"
+#include "GameEngine.hpp"
+#include "Bitmap.hpp"
+
+const int g_MAXSPEED = 8;
+
+std::unique_ptr<GameEngine> g_game;
+std::unique_ptr<Bitmap>     g_background;
+std::unique_ptr<Bitmap>     g_saucer;
+int                         g_saucerX;
+int                         g_saucerY;
+int                         g_speedX;
+int                         g_speedY;
