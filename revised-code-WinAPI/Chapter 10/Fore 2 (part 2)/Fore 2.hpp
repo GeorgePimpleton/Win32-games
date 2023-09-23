57184b23966186a64d04d103cce0d9d998c1ad8f
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-23 08:53:13 -0700

    Fore 2

---
+++ revised-code-WinAPI/Chapter 10/Fore 2 (part 2)/Fore 2.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <windows.h>
+#include <memory>
+#include <vector>
+#include "resource.h"
+#include "Bitmap.hpp"
+#include "Sprite.hpp"
+#include "GameEngine.hpp"
+
+std::unique_ptr<GameEngine> g_game;
+std::unique_ptr<Bitmap>     g_forestBitmap;
+std::unique_ptr<Bitmap>     g_golfBallBitmap;
+HDC                         g_offscreenDC;
+HBITMAP                     g_offscreenBitmap;
+Sprite*                     g_dragSprite;
---
+++ revised-code-WinAPI/Chapter 10/Fore 2 (part 2)/random_toolkit.hpp
@@ -0,0 +1,109 @@
+/* A simple toolkit to help beginners using the <random> library an easier task
+ *
+ * pre-C++20 header file */
+
+ // v1.3.1
+
+  // shamelessly stolen and adapted from a C++ working paper: WG21 N3551
+  // http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3551.pdf
+
+#ifndef RANDOM_TOOLKIT
+#define RANDOM_TOOLKIT
+
+#include <chrono>
+#include <random>
+#include <stdexcept>
+#include <limits>
+
+namespace rtk
+{
+   static bool seeded { false };
+
+   inline std::mt19937& urng( )
+   {
+      static std::mt19937 URNG { };
+
+      return URNG;
+   }
+
+   // function to initialize (seed) the PRNG engine
+   // uses a seed sequence for better randomization
+   inline void srand(bool FORCE_SEED = false)
+   {
+      static const std::seed_seq::result_type seeds[ ] { std::random_device {} (),
+                                                         std::seed_seq::result_type(std::chrono::system_clock::now( ).time_since_epoch( ).count( )) };
+
+      static std::seed_seq sseq(std::begin(seeds), std::end(seeds));
+
+      // the URNG can't be reseeded unless forced
+      if ( !seeded || FORCE_SEED )
+      {
+         urng( ).seed(sseq);
+
+         seeded = true;
+      }
+   }
+
+   // function to initialize the PRNG engine
+   // using a specified seed
+   inline void srand(signed seed, bool FORCE_SEED = false)
+   {
+      // the URNG can't be reseeded unless forced
+      if ( !seeded || FORCE_SEED )
+      {
+         urng( ).seed(static_cast<unsigned>(seed));
+
+         seeded = true;
+      }
+   }
+
+   // two functions to obtain uniform distribution ints and doubles
+   inline int rand_int(int from, int to)
+   {
+      static std::uniform_int_distribution<> dist { };
+
+      if ( from > to ) { throw std::invalid_argument("bad int distribution params"); }
+
+      return dist(urng( ), decltype(dist)::param_type { from, to });
+   }
+
+   inline double rand_rl(double from, double to)
+   {
+      static std::uniform_real_distribution<> dist { };
+
+      // a real distribution kinda goes flakey when the params are equal, divide by zero will do that,
+      // as well as reversed from expected
+      // if ( !(from < to && ((to - from) <= std::numeric_limits<double>::max( ))) )
+      if ( !(from < to && ((to - from) <= DBL_MAX)) ) // The WinAPI gets all whingey about the C++ max function()
+      {
+         throw std::invalid_argument("bad double distribution params");
+      }
+
+      return dist(urng( ), decltype(dist)::param_type { from, to });
+   }
+
+   // two function overloads to obtain uniform distribution ints and doubles
+   inline int rand(int from, int to)
+   {
+      return rand_int(from, to);
+   }
+
+   inline double rand(double from, double to)
+   {
+      return rand_rl(from, to);
+   }
+
+   // function for rolling dice, and checking if the # of pips is nonstandard
+   inline int roll_die(int pips)
+   {
+      // check to see if the number of die pips is less than 2
+      if ( pips < 2 )
+      {
+         return -1;
+      }
+
+      return rand(1, pips);
+   }
+}
+
+#endif
