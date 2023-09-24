fef995553c8ca4f3c52873ed7c21da577ab6b6dc
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-24 13:10:36 -0700

    Meteor Defense

---
+++ revised-code-WinAPI/Chapter 17/Meteor Defense (part 2)/Background.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <windows.h>
+#include "random_toolkit.hpp"
+#include "Bitmap.hpp"
+#include <array>
+
+enum class STARSIZE : WORD { SS_SMALL  = 0,
+                             SS_MEDIUM = 1,
+                             SS_LARGE  = 3 };
+
+using enum STARSIZE;
+
+class Background
+{
+protected:
+   Bitmap*  m_bitmap;
+   COLORREF m_color;
+   int      m_width;
+   int      m_height;
+
+public:
+            Background(int width, int height, COLORREF color);
+            Background(Bitmap* bitmap);
+   virtual ~Background( );
+
+   virtual void Update( );
+   virtual void Draw(HDC hDC);
+
+   int GetWidth( ) const  { return m_width; };
+   int GetHeight( ) const { return m_height; };
+};
+
+class StarryBackground : Background
+{
+protected:
+   int                       m_numStars;
+   int                       m_twinkleDelay;
+   std::array<POINT, 100>    m_stars;
+   std::array<COLORREF, 100> m_starColors;
+   std::array<STARSIZE, 100> m_sizeStars;
+
+public:
+            StarryBackground(int width, int height,
+                             int numStars = 100,
+                             int twinkleDelay = 50);
+   virtual ~StarryBackground( );
+
+   virtual void Update( );
+   virtual void Draw(HDC hDC);
+};
---
+++ revised-code-WinAPI/Chapter 17/Meteor Defense (part 2)/MeteorDefense.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <windows.h>
+#include <memory>
+#include "resource.h"
+#include "random_toolkit.hpp"
+#include "GameEngine.hpp"
+#include "Bitmap.hpp"
+#include "Sprite.hpp"
+#include "Background.hpp"
+
+HDC                               g_offscreenDC;
+HBITMAP                           g_offscreenBitmap;
+std::unique_ptr<GameEngine>       g_game;
+std::unique_ptr<Bitmap>           g_groundBitmap;
+std::unique_ptr<Bitmap>           g_cityBitmap;
+std::unique_ptr<Bitmap>           g_meteorBitmap;
+std::unique_ptr<Bitmap>           g_missileBitmap;
+std::unique_ptr<Bitmap>           g_explosionBitmap;
+std::unique_ptr<Bitmap>           g_gameOverBitmap;
+std::unique_ptr<StarryBackground> g_background;
+int                               g_numCities;
+int                               g_score;
+int                               g_difficulty;
+BOOL                              g_gameOver;
+
+void AddMeteor();
