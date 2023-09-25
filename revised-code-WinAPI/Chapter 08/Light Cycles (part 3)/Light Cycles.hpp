eed16f7fadc6c6e27ce1a5af4de20b812ffc0400
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-25 06:56:02 -0700

    Light Cycles

--- revised-code-WinAPI/Chapter 08/Light Cycles (part 3)/Light Cycles.hpp
+++ revised-code-WinAPI/Chapter 08/Light Cycles (part 3)/Light Cycles.hpp
@@ -9,16 +9,16 @@
 #include "Bitmap.hpp"
 #include "random_toolkit.hpp"
 
-const LONG                      g_SPEED = 4;
+const LONG g_SPEED = 4;
 
-std::unique_ptr<GameEngine>     g_game;
-std::unique_ptr<Bitmap>         g_background;
-std::array<std::array<std::unique_ptr<Bitmap>, 4>, 2>         g_cycle;
-std::vector<POINT>              g_cyclePos(2);
-std::vector<POINT>              g_cycleSpeed(2);
-std::vector<std::vector<POINT>> g_cycleTrail(2, std::vector<POINT>(100));
-std::vector<LONG>               g_trailLength(2);
-BOOL                            g_gameOver;
+std::unique_ptr<GameEngine>                           g_game;
+std::unique_ptr<Bitmap>                               g_background;
+std::array<std::array<std::unique_ptr<Bitmap>, 4>, 2> g_cycle;
+std::vector<POINT>                                    g_cyclePos(2);
+std::vector<POINT>                                    g_cycleSpeed(2);
+std::vector<std::vector<POINT>>                       g_cycleTrail(2, std::vector<POINT>(100));
+std::vector<LONG>                                     g_trailLength(2);
+BOOL                                                  g_gameOver;
 
 // game specific functions
 void NewGame( );
