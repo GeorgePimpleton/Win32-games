5d0ee6ac46e6f609730f03adaaf9dcae93a9ce20
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-25 07:42:52 -0700

    Space Out 2

---
+++ revised-code-WinAPI/Chapter 20/Space Out 2 (part 1)/SpaceOut.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <windows.h>
+#include "resource.h"
+#include "GameEngine.h"
+#include "Bitmap.h"
+#include "Sprite.h"
+#include "Background.h"
+#include "AlienSprite.h"
+
+HINSTANCE         g_hInstance;
+GameEngine*       g_pGame;
+HDC               g_hOffscreenDC;
+HBITMAP           g_hOffscreenBitmap;
+Bitmap*           g_pSplashBitmap;
+Bitmap*           g_pDesertBitmap;
+Bitmap*           g_pCarBitmap;
+Bitmap*           g_pSmCarBitmap;
+Bitmap*           g_pMissileBitmap;
+Bitmap*           g_pBlobboBitmap;
+Bitmap*           g_pBMissileBitmap;
+Bitmap*           g_pJellyBitmap;
+Bitmap*           g_pJMissileBitmap;
+Bitmap*           g_pTimmyBitmap;
+Bitmap*           g_pTMissileBitmap;
+Bitmap*           g_pSmExplosionBitmap;
+Bitmap*           g_pLgExplosionBitmap;
+Bitmap*           g_pGameOverBitmap;
+StarryBackground* g_pBackground;
+Sprite*           g_pCarSprite;
+int               g_iFireInputDelay;
+int               g_iNumLives, g_iScore, g_iDifficulty;
+BOOL              g_bGameOver;
+BOOL              g_bSplash;
+
+void NewGame( );
+void AddAlien( );
