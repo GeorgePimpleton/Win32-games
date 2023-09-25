0bfd12245cad4381fd2cbfae32e89f7e303d741b
George Pimpleton <george_pimpleton@hotmail.com>
2023-09-25 10:02:13 -0700

    Wanderer

--- revised-code-WinAPI/Chapter 23/Wanderer (part 1)/PersonSprite.cpp
+++ revised-code-WinAPI/Chapter 23/Wanderer (part 1)/PersonSprite.cpp
@@ -1,38 +1,17 @@
-//-----------------------------------------------------------------
-// Person Sprite Object
-// C++ Source - PersonSprite.cpp
-//-----------------------------------------------------------------
-
-//-----------------------------------------------------------------
-// Include Files
-//-----------------------------------------------------------------
 #include "PersonSprite.h"
 
-//-----------------------------------------------------------------
-// PersonSprite Constructor(s)/Destructor
-//-----------------------------------------------------------------
-PersonSprite::PersonSprite(Bitmap* pBitmap, RECT& rcBounds,
-  BOUNDSACTION baBoundsAction) : Sprite(pBitmap, rcBounds,
-  baBoundsAction)
-{
-}
+PersonSprite::PersonSprite(Bitmap* pBitmap, RECT& rcBounds, BOUNDSACTION baBoundsAction)
+   : Sprite(pBitmap, rcBounds, baBoundsAction)
+{ }
 
-PersonSprite::~PersonSprite()
-{
-}
+PersonSprite::~PersonSprite( )
+{ }
 
-//-----------------------------------------------------------------
-// PersonSprite Virtual Methods
-//-----------------------------------------------------------------
-inline void PersonSprite::UpdateFrame()
-{
-}
+inline void PersonSprite::UpdateFrame( )
+{ }
 
-//-----------------------------------------------------------------
-// PersonSprite General Methods
-//-----------------------------------------------------------------
-void PersonSprite::Walk()
+void PersonSprite::Walk( )
 {
-  // Toggle between the two frames to give the illusion of walking
-  m_iCurFrame = 1 - m_iCurFrame;
+   // toggle between the two frames to give the illusion of walking
+   m_iCurFrame = 1 - m_iCurFrame;
 }
--- revised-code-WinAPI/Chapter 23/Wanderer (part 1)/Wanderer.h
+++ revised-code-WinAPI/Chapter 23/Wanderer (part 1)/Wanderer.h
@@ -1,13 +1,5 @@
-//-----------------------------------------------------------------
-// Wanderer Application
-// C++ Header - Wanderer.h
-//-----------------------------------------------------------------
-
 #pragma once
 
-//-----------------------------------------------------------------
-// Include Files
-//-----------------------------------------------------------------
 #include <windows.h>
 #include "Resource.h"
 #include "GameEngine.h"
@@ -16,17 +8,14 @@
 #include "ScrollingBackground.h"
 #include "PersonSprite.h"
 
-//-----------------------------------------------------------------
-// Global Variables
-//-----------------------------------------------------------------
-HINSTANCE             g_hInstance;
-GameEngine*           g_pGame;
-HDC                   g_hOffscreenDC;
-HBITMAP               g_hOffscreenBitmap;
-BackgroundLayer*      g_pBGLandscapeLayer;
-BackgroundLayer*      g_pFGCloudsLayer;
-ScrollingBackground*  g_pBackground;
-ScrollingBackground*  g_pForeground;
-Bitmap*               g_pPersonBitmap;
-PersonSprite*         g_pPersonSprite;
-int                   g_iInputDelay;
+HINSTANCE            g_hInstance;
+GameEngine*          g_pGame;
+HDC                  g_hOffscreenDC;
+HBITMAP              g_hOffscreenBitmap;
+BackgroundLayer*     g_pBGLandscapeLayer;
+BackgroundLayer*     g_pFGCloudsLayer;
+ScrollingBackground* g_pBackground;
+ScrollingBackground* g_pForeground;
+Bitmap*              g_pPersonBitmap;
+PersonSprite*        g_pPersonSprite;
+int                  g_iInputDelay;
