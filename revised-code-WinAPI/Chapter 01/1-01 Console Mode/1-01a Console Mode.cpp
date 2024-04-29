1d5d6304eb914cff74c711db12032f764a1004c7
George Pimpleton <george_pimpleton@hotmail.com>
2024-04-29 09:53:51 -0700

    Basic Programming Primer, Console and WinAPI
    Revamp dir layout

---
+++ revised-code-WinAPI/Chapter 01/1-01 Console Mode/1-01a Console Mode.cpp
@@ -0,0 +1,9 @@
+// include necessary libraries
+#include <iostream>
+
+// execution begins here
+int main( )
+{
+   // output a message to the console window
+   std::cout << "Hello World!" << std::endl;
+}
